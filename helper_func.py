e6174bb10884105d302a1fe601900a9e1872ae0b
H1kki05 <v15hnuf6n1@yahoo.com>
2024-08-09 22:27:01 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -95,7 +95,7 @@ async def get_verify_status(user_id):
     verify = await db_verify_status(user_id)
     return verify
 
-async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=0, link=""):
+async def update_verify_status(user_id, verify_token="", is_verified=True, verified_time=0, link=""):
     current = await db_verify_status(user_id)
     current['verify_token'] = verify_token
     current['is_verified'] = is_verified
